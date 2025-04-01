311a795c10081711a0a9f38f4b316065a183c690
aruraruri <aurilait@gmail.com>
2025-04-01 11:34:23 +0300

    Add sharp dependency and implement thumbnail creation middleware

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,4 +1,15 @@
-export function createThumbnail(req, res, next) {
-  console.log('todo: tee kuvakäsittely');
+import sharp from 'sharp';
+
+export async function createThumbnail(req, res, next) {
+  console.log('todo: tee kuvakäsittely', req.file);
+  if (!req.file) {
+    next('image not found in req');
+    return;
+  }
+
+  await sharp(req.file.path)
+    .resize({width: 100, height: 100})
+    .toFile(`${req.file.path}_thumb`);
+
   next();
 }
