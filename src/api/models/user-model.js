2df73fdc65ae327b7760d86fc9ff3d853a52bc0a
aruraruri <aurilait@gmail.com>
2025-04-01 09:35:41 +0300

    Rename cat API constituents

---
+++ src/api/models/user-model.js
@@ -0,0 +1,18 @@
+const userItems = [
+  {
+    user_id: 3609,
+    name: 'John Doe',
+    username: 'johndoe',
+    email: 'john@metropolia.fi',
+    role: 'user',
+    password: 'password',
+  },
+  {
+    user_id: 3609,
+    name: 'Liisa',
+    username: 'liisa',
+    email: 'liisa@metropolia.fi',
+    role: 'user',
+    password: 'password',
+  },
+];
